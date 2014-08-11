cb08dc647f3e307df95d412f69ef2881fcf1bd4e
neniverena <neniverena@gmx.de>
2014-08-11 15:36:31 +0200

    Player klasse geändert

--- src/de/ur/unimon/actionbar/Player.java
+++ src/de/ur/unimon/actionbar/Player.java
@@ -11,17 +11,38 @@ public class Player {
 	//Inventory inventory = new Inventory (0, 0, 0, 0);
 	
 	public Player () {
-		this.ownedMoney = 0;
+		ownedMoney = 0;
 //		inventory = new Inventory (0, 0, 0, 0);
 		ownUnimonList = new ArrayList<Unimon>();
 	}
 	
 	public Player (int Money, ArrayList<Unimon> ownUnimonList) {
-		
+//	public Player (int Money, ArrayList<Unimon> ownUnimonList, Inventory inventory) {
+		ownedMoney = Money; 
+		this.ownUnimonList = ownUnimonList; 
+//		this.inventory = inventory;
 	}
 
 	public void addMoney(int newMoney) {
 		ownedMoney += newMoney; 
 	}
-
+	
+	public int getMoney() {
+		return ownedMoney;
+	}
+	
+	public void addUnimon(Unimon newUnimon) {
+		ownUnimonList.add(newUnimon);
+	}
+	
+	public ArrayList<Unimon> getUnimonList () {
+		return ownUnimonList;
+	}
+	
+//	public void addToInventory() {
+//		
+//	}
+//	public Inventory getInventory() {
+//		return inventory;
+//	}
 }
