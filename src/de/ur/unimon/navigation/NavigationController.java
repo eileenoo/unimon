11de4c136773b9f6faf02870b0727111e54ad3d0
Brunzkoata <user@132.199.201.88>
2014-08-13 17:17:43 +0200

    TrainerList + Trainer-Klasse

--- src/de/ur/unimon/navigation/NavigationController.java
+++ src/de/ur/unimon/navigation/NavigationController.java
@@ -1,11 +1,5 @@
 package de.ur.unimon.navigation;
 
-import android.location.Location;
-import android.location.LocationListener;
-
-public class NavigationController implements LocationListener {
-	Location location;
-	
-	
+public class NavigationController {
 
 }
