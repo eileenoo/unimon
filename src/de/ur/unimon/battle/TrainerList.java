f6684afa024adbdbaa552147acfa9598bc17e816
Brunzkoata <user@132.199.201.88>
2014-08-13 17:20:53 +0200

    Trainer-Klasse + TrainerListe

--- src/de/ur/unimon/battle/TrainerList.java
+++ src/de/ur/unimon/battle/TrainerList.java
@@ -1,12 +1,13 @@
 package de.ur.unimon.battle;
 
+
 import java.util.ArrayList;
 
 import de.ur.unimon.unimons.Unimon;
 import de.ur.unimon.unimons.UnimonList;
 
 public class TrainerList {
-
+	
 	private static Trainer trainer1;
 	private static Trainer trainer2;
 	private static Trainer trainer3;
@@ -16,7 +17,7 @@ public class TrainerList {
 	private static Trainer trainer7;
 	private static Trainer boss;
 	public ArrayList<Trainer> TrainerList;
-
+		
 	public Unimon leckMichAmArsch(int loch) {
 		UnimonList allUnimonsList = new UnimonList();
 		ArrayList<Unimon> unimonList = new ArrayList<Unimon>();
@@ -24,23 +25,16 @@ public class TrainerList {
 		return unimonList.get(loch);
 	}
 
-	public TrainerList() {
-		trainer1 = new Trainer(1, "BöserBube1", 48.9979054, 12.0957183,
-				leckMichAmArsch(0), 70, 70);
-		trainer2 = new Trainer(2, "BöserBube2", 48.9979054, 12.0957183,
-				leckMichAmArsch(1), 150, 150);
-		trainer3 = new Trainer(3, "BöserBube3", 48.9979054, 12.0957183,
-				leckMichAmArsch(2), 250, 250);
-		trainer4 = new Trainer(4, "BöserBube4", 48.9979054, 12.0957183,
-				leckMichAmArsch(2), 350, 350);
-		trainer5 = new Trainer(5, "BöserBube5", 48.9979054, 12.0957183,
-				leckMichAmArsch(2), 150, 150);
-		trainer6 = new Trainer(6, "BöserBube6", 48.9979054, 12.0957183,
-				leckMichAmArsch(2), 150, 150);
-		trainer7 = new Trainer(7, "BöserBube7", 48.9979054, 12.0957183,
-				leckMichAmArsch(2), 150, 150);
-		boss = new Trainer(2, "OberböserBube", 48.9979054, 12.0957183,
-				leckMichAmArsch(3), 150, 150);
+	public TrainerList(){
+		TrainerList = new ArrayList<Trainer>();
+		trainer1 = new Trainer(1, "BöserBube1", 48.9979054, 12.0957183, leckMichAmArsch(0), 70, 70);
+		trainer2 = new Trainer(2, "BöserBube2", 48.9979054, 12.0957183, leckMichAmArsch(1), 150, 150);
+		trainer3 = new Trainer(3, "BöserBube3", 48.9979054, 12.0957183, leckMichAmArsch(2), 250, 250);
+		trainer4 = new Trainer(4, "BöserBube4", 48.9979054, 12.0957183, leckMichAmArsch(2), 350, 350);
+		trainer5 = new Trainer(5, "BöserBube5", 48.9979054, 12.0957183, leckMichAmArsch(2), 150, 150);
+		trainer6 = new Trainer(6, "BöserBube6", 48.9979054, 12.0957183, leckMichAmArsch(2), 150, 150);
+		trainer7 = new Trainer(7, "BöserBube7", 48.9979054, 12.0957183, leckMichAmArsch(2), 150, 150);
+		boss = new Trainer(2, "OberböserBube", 48.9979054, 12.0957183, leckMichAmArsch(3), 150, 150);
 		TrainerList.add(trainer1);
 		TrainerList.add(trainer2);
 		TrainerList.add(trainer3);
