4667e9d4a5a22fa9257cb04b08a3bb3be66da237
Brunzkoata <user@132.199.201.88>
2014-08-21 16:37:37 +0200

    Trainer Koordinaten + HospitalActivity

--- src/de/ur/unimon/battle/TrainerList.java
+++ src/de/ur/unimon/battle/TrainerList.java
@@ -21,13 +21,13 @@ public class TrainerList {
 	public TrainerList(){
 		unimonList = new UnimonList().getUnimonList();
 		trainerList = new ArrayList<Trainer>();
-		trainer1 = new Trainer(1, "BöserBube1", 48.9979054, 12.0957183, unimonList.get(0), 70, 70);
-		trainer2 = new Trainer(2, "BöserBube2", 48.9979054, 12.0957183, unimonList.get(1), 150, 150);
-		trainer3 = new Trainer(3, "BöserBube3", 48.9979054, 12.0957183, unimonList.get(2), 250, 250);
-		trainer4 = new Trainer(4, "BöserBube4", 48.9979054, 12.0957183, unimonList.get(0), 350, 350);
-		trainer5 = new Trainer(5, "BöserBube5", 48.9979054, 12.0957183, unimonList.get(1), 150, 150);
-		trainer6 = new Trainer(6, "BöserBube6", 48.9979054, 12.0957183, unimonList.get(2), 150, 150);
-		boss = new Trainer(2, "OberböserBube", 48.9979054, 12.0957183, unimonList.get(0), 150, 150);
+		trainer1 = new Trainer(1, "Wirtschaft", 48.9987501, 12.0936204, unimonList.get(0), 70, 70);
+		trainer2 = new Trainer(2, "Vielberth", 49.0004036, 12.0942936, unimonList.get(1), 150, 150);
+		trainer3 = new Trainer(3, "rechtsVonBib", 48.9979429, 12.0973199, unimonList.get(2), 250, 250);
+		trainer4 = new Trainer(4, "Brücke", 48.9971498, 12.0951285, unimonList.get(0), 350, 350);
+		trainer5 = new Trainer(5, "Chemie", 48.9956951, 12.0952814, unimonList.get(1), 150, 150);
+		trainer6 = new Trainer(6, "Mathe", 48.997165, 12.0934727, unimonList.get(2), 150, 150);
+		boss = new Trainer(7, "Stadion", 48.9933939, 12.0966834, unimonList.get(0), 150, 150);
 		trainerList.add(trainer1);
 		trainerList.add(trainer2);
 		trainerList.add(trainer3);
